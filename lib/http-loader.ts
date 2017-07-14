d616ba2a0e922675614baee9a47b9b57015df847
Maksym Beresnev <maksym.beresnev@novage.com.ua>
2017-07-14 16:15:13 +0300

    HttpLoader class functionality: - file loading in queue with order until queue end; - simultaneous file loading; - tracks downloaded files; - emits file response if file already downloaded; - cancel the loading of the old file queue.

--- lib/http-loader.ts
+++ lib/http-loader.ts
@@ -1,37 +1,103 @@
 import LoaderInterface from "./loader-interface";
-import {EventEmitter} from "events";
 import LoaderFile from "./loader-file";
+import {EventEmitter} from "events";
 
 export default class HttpLoader extends EventEmitter implements LoaderInterface {
 
-    private fileQueue: LoaderFile[];
+    private readonly simultaneousLoads = 2;
+    private xhrRequests: Map<string, XMLHttpRequest> = new Map();
+    private fileQueue: LoaderFile[] = [];
+    private downloadedFiles: LoaderFile[] = [];
 
     public constructor() {
         super();
     }
 
+    /**
+     * Adds files to the download queue.
+     * Cancels the download of previous ones if they are not in the new queue.
+     * The result of the downloading will be emitted via file_loaded or file_error events.
+     *
+     * @param {LoaderFile[]} files Files to download.
+     */
     public load(files: LoaderFile[]): void {
-        this.fileQueue = [...files];
+
+        // stop all xhr requests for files that are not in the new load
         this.fileQueue.forEach((file) => {
-            this.loadFile(file);
+            if (files.findIndex((f) => f.url === file.url) === -1) {
+                const xhr = this.xhrRequests.get(file.url);
+                if (xhr) {
+                    xhr.abort();
+                    this.xhrRequests.delete(file.url);
+                }
+            }
+        });
+
+        // renew file queue
+        this.fileQueue = [...files];
+
+        // emit file loaded event if the file has already been downloaded
+        this.fileQueue.forEach((file) => {
+            const downloadedFile = this.downloadedFiles.find((f) => f.url === file.url);
+            if (downloadedFile) {
+                this.emit("file_loaded", downloadedFile);
+            }
+        });
+
+        this.loadFileQueue();
+    }
+
+    /**
+     * Loops through the files queue and starts file loading. Also saves an instance of XMLHttpRequest of the uploaded file.
+     *
+     * Starts downloading if:
+     * - number of simultaneous downloads not exceeded;
+     * - downloading of this file has not started yet;
+     * - the file is not in the list of downloaded files.
+     */
+    private loadFileQueue(): void {
+        this.fileQueue.forEach((file) => {
+            if (this.xhrRequests.size < this.simultaneousLoads &&
+                !this.xhrRequests.has(file.url) && this.downloadedFiles.findIndex((f) => f.url === file.url) === -1) {
+
+                this.xhrRequests.set(file.url, this.loadFile(file));
+            }
         });
     }
 
-    private loadFile(file: LoaderFile) {
+    /**
+     * Loads the specified file.
+     *
+     * @param {LoaderFile} file File to download.
+     * @returns {XMLHttpRequest} An instance of XMLHttpRequest.
+     */
+    private loadFile(file: LoaderFile): XMLHttpRequest {
         const request = new XMLHttpRequest();
         request.open("GET", file.url, true);
         request.responseType = "arraybuffer";
 
         request.onload = (event: any) => {
+            this.xhrRequests.delete(file.url);
+
             if (event.target.status === 200) {
                 file.data = event.target.response;
+                this.downloadedFiles.push(file);
                 this.emit("file_loaded", file);
             } else {
-                console.warn("The file could not be loaded", event);
+                this.emit("file_error", event);
             }
+
+            this.loadFileQueue();
+        };
+
+        request.onerror = (event: any) => {
+            this.xhrRequests.delete(file.url);
+            this.emit("file_error", event);
+            this.loadFileQueue();
         };
 
         request.send();
+        return request;
     }
 
 }
