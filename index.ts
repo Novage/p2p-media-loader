3841b795f1cc442651421e34cc32effa17f4e055
Maksym Beresnev <maksym.beresnev@novage.com.ua>
2017-08-08 15:00:05 +0300

    The project is prepared for use in the NPM.

--- index.ts
+++
@@ -1,7 +0,0 @@
-import HttpLoader from "./lib/http-loader";
-import ChunkManager from "./lib/chunk-manager";
-import P2PMediaLoader from "./lib/p2p-media-loader";
-
-module.exports = P2PMediaLoader;
-module.exports.HttpLoader = HttpLoader;
-module.exports.ChunkManager = ChunkManager;
---
+++ lib/index.ts
@@ -0,0 +1,3 @@
+export {default as P2PMediaLoader} from "./p2p-media-loader";
+export {default as HttpLoader} from "./http-loader";
+export {default as ChunkManager} from "./chunk-manager";
