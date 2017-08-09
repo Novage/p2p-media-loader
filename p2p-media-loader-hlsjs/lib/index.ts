2cb3a6ec9357d62282dfa4f68f0f486889c0d34a
Yuriy Grinevich <yuriy.grinevich@novage.com.ua>
2017-08-09 16:31:11 +0300

    Reworked p2pml.hlsjs API

--- p2p-media-loader-hlsjs/lib/index.ts
+++ p2p-media-loader-hlsjs/lib/index.ts
@@ -3,12 +3,17 @@ import ChunkManager from "./chunk-manager";
 import HlsJsLoader from "./hlsjs-loader";
 const getHlsJsLoaderMaker = require("./hlsjs-loader-maker");
 
-export {HybridLoader} from "p2p-media-loader-core";
-
-export function getLoader() {
+export function initPlayer(player: any) {
     const hybridLoader = new HybridLoader();
     const chunkManager = new ChunkManager(hybridLoader);
-    return getHlsJsLoaderMaker(HlsJsLoader, chunkManager);
+    const hlsjsLoader = getHlsJsLoaderMaker(HlsJsLoader, chunkManager);
+
+    player.config.loader = hlsjsLoader;
+
+    player.on("hlsFragChanged", function (event: any, data: any) {
+        const url = data && data.frag ? data.frag.url : undefined;
+        chunkManager.setCurrentChunk(url);
+    });
 }
 
 export {default as ChunkManager} from "./chunk-manager";
