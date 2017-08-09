790f539d8519e00f090b24c153f1663af52c8315
Maksym Beresnev <maksym.beresnev@novage.com.ua>
2017-08-09 12:02:23 +0300

    Splitting core and hlsjs projects

--- p2p-media-loader-core/lib/index.ts
+++ p2p-media-loader-core/lib/index.ts
@@ -1,3 +1,2 @@
-export {default as P2PMediaLoader} from "./p2p-media-loader";
 export {default as HttpLoader} from "./http-loader";
-export {default as ChunkManager} from "./chunk-manager";
+export {default as HybridLoader} from "./hybrid-loader";
