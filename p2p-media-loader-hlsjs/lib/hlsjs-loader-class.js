4a61f8d6f9301974b0c2401e0548fdd865c108ba
Yuriy Grinevich <yuriy.grinevich@novage.com.ua>
2017-08-09 17:41:58 +0300

    Reworked p2pml.hlsjs API

---
+++ p2p-media-loader-hlsjs/lib/hlsjs-loader-class.js
@@ -0,0 +1,26 @@
+function getHlsJsLoaderClass(HlsJsLoader, chunkManager) {
+    function HlsJsLoaderClass(settings) {
+        this.impl = new HlsJsLoader(chunkManager, settings);
+        this.stats = this.impl.stats;
+    }
+
+    HlsJsLoaderClass.prototype.load = function (context, config, callbacks) {
+        return this.impl.load(context, config, callbacks);
+    };
+
+    HlsJsLoaderClass.prototype.abort = function () {
+        return this.impl.abort();
+    };
+
+    HlsJsLoaderClass.prototype.destroy = function () {
+        return this.impl.destroy();
+    };
+
+    HlsJsLoaderClass.getChunkManager = function () {
+        return chunkManager;
+    };
+
+    return HlsJsLoaderClass;
+}
+
+module.exports = getHlsJsLoaderClass;
--- p2p-media-loader-hlsjs/lib/index.ts
+++ p2p-media-loader-hlsjs/lib/index.ts
@@ -1,12 +1,24 @@
 import {HybridLoader} from "p2p-media-loader-core";
 import ChunkManager from "./chunk-manager";
 import HlsJsLoader from "./hlsjs-loader";
-const getHlsJsLoaderMaker = require("./hlsjs-loader-maker");
+const createHlsJsLoaderClass = require("./hlsjs-loader-class");
 
-export function initPlayer(player: any) {
-    const hybridLoader = new HybridLoader();
-    const chunkManager = new ChunkManager(hybridLoader);
-    const hlsjsLoader = getHlsJsLoaderMaker(HlsJsLoader, chunkManager);
+export function createLoaderClass(chunkManager: ChunkManager) {
+    return createHlsJsLoaderClass(HlsJsLoader, chunkManager);
+}
+
+export function initPlayer(player: any, loaderClass?: any) {
+    let hlsjsLoader: any;
+    let chunkManager: ChunkManager;
+
+    if (loaderClass) {
+        hlsjsLoader = loaderClass;
+        chunkManager = loaderClass.getChunkManager();
+    } else {
+        const hybridLoader = new HybridLoader();
+        chunkManager = new ChunkManager(hybridLoader);
+        hlsjsLoader = createLoaderClass(chunkManager);
+    }
 
     player.config.loader = hlsjsLoader;
 
