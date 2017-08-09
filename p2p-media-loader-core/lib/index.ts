106c6605d2cdbc40cecf9ad24b53d54af1e6575f
Maksym Beresnev <maksym.beresnev@novage.com.ua>
2017-08-09 13:26:08 +0300

    Export LoaderInterface

--- p2p-media-loader-core/lib/index.ts
+++ p2p-media-loader-core/lib/index.ts
@@ -1,2 +1,5 @@
+export {default as LoaderInterface} from "./loader-interface";
+export {default as LoaderEvents} from "./loader-events";
+export {default as LoaderFile} from "./loader-file";
 export {default as HttpLoader} from "./http-loader";
 export {default as HybridLoader} from "./hybrid-loader";
