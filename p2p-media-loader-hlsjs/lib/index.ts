06084d3cf19d0365f189b87860f260d64a1144b5
Maksym Beresnev <maksym.beresnev@novage.com.ua>
2017-08-10 12:53:28 +0300

    Simplified hlsjs player configuration API

--- p2p-media-loader-hlsjs/lib/index.ts
+++ p2p-media-loader-hlsjs/lib/index.ts
@@ -3,28 +3,20 @@ import ChunkManager from "./chunk-manager";
 import HlsJsLoader from "./hlsjs-loader";
 const createHlsJsLoaderClass = require("./hlsjs-loader-class");
 
-export function createLoaderClass(chunkManager: ChunkManager) {
-    return createHlsJsLoaderClass(HlsJsLoader, chunkManager);
-}
-
-export function initPlayer(player: any, loaderClass?: any) {
-    let hlsjsLoader: any;
-    let chunkManager: ChunkManager;
-
-    if (loaderClass) {
-        hlsjsLoader = loaderClass;
-        chunkManager = loaderClass.getChunkManager();
-    } else {
+export function initPlayer(player: any, chunkManager?: ChunkManager) {
+    if (!chunkManager) {
         const hybridLoader = new HybridLoader();
         chunkManager = new ChunkManager(hybridLoader);
-        hlsjsLoader = createLoaderClass(chunkManager);
     }
+
+    let hlsjsLoader = createHlsJsLoaderClass(HlsJsLoader, chunkManager);
 
     player.config.loader = hlsjsLoader;
 
+    const chunkManagerForCallback: ChunkManager = chunkManager;
     player.on("hlsFragChanged", function (event: any, data: any) {
         const url = data && data.frag ? data.frag.url : undefined;
-        chunkManager.setCurrentChunk(url);
+        chunkManagerForCallback.setCurrentChunk(url);
     });
 }
 
