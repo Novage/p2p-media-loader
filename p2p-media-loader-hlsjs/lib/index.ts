fe98852a98b9a59995b8a5a9d84ba791607f8ed3
Yuriy Grinevich <yuriy.grinevich@novage.com.ua>
2017-08-10 13:21:45 +0300

    Removed wrongly committed code

--- p2p-media-loader-hlsjs/lib/index.ts
+++ p2p-media-loader-hlsjs/lib/index.ts
@@ -4,29 +4,12 @@ import HlsJsLoader from "./hlsjs-loader";
 const createHlsJsLoaderClass = require("./hlsjs-loader-class");
 
 export function initPlayer(player: any, settings: any = {}) {
-    let chunkManager: ChunkManager = settings.chunkManager || new ChunkManager(new HybridLoader());
+    const chunkManager: ChunkManager = settings.chunkManager || new ChunkManager(new HybridLoader());
     player.config.loader = createHlsJsLoaderClass(HlsJsLoader, chunkManager);
 
     player.on("hlsFragChanged", function (event: any, data: any) {
         const url = data && data.frag ? data.frag.url : undefined;
         chunkManager.setCurrentChunk(url);
-    });
-}
-
-export function getChunkManager(player: any, chunkManager?: ChunkManager) {
-    if (!chunkManager) {
-        const hybridLoader = new HybridLoader();
-        chunkManager = new ChunkManager(hybridLoader);
-    }
-
-    let hlsjsLoader = createHlsJsLoaderClass(HlsJsLoader, chunkManager);
-
-    player.config.loader = hlsjsLoader;
-
-    const chunkManagerForCallback: ChunkManager = chunkManager;
-    player.on("hlsFragChanged", function (event: any, data: any) {
-        const url = data && data.frag ? data.frag.url : undefined;
-        chunkManagerForCallback.setCurrentChunk(url);
     });
 }
 
