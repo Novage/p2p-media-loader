ced551f3fbe0099cad3eea0725c493aa688f461d
Maksym Beresnev <maksym.beresnev@novage.com.ua>
2017-08-23 15:50:43 +0300

    Added window.p2pml.WEBRTC_SUPPORT flag

--- p2p-media-loader-core/lib/browser-init.js
+++ p2p-media-loader-core/lib/browser-init.js
@@ -1,1 +1,4 @@
 window.p2pml = require("p2p-media-loader-core");
+
+var browserRTC = require('get-browser-rtc')();
+window.p2pml.WEBRTC_SUPPORT = !!browserRTC && browserRTC.RTCPeerConnection.prototype.createDataChannel !== undefined;
