4db7e4743ffb0bd2099b4a787ec179a50ebf5352
Yuriy Grinevich <yuriy.grinevich@novage.com.ua>
2018-07-24 13:30:55 +0300

    Core: tests fixed

--- p2p-media-loader-core/test/hybrid-loader.test.ts
+++ p2p-media-loader-core/test/hybrid-loader.test.ts
@@ -3,9 +3,9 @@
 /// <reference path="../node_modules/typescript/lib/lib.es2015.d.ts" />
 /// <reference path="../node_modules/typescript/lib/lib.dom.d.ts" />
 
-import HttpMediaManager from "../lib/http-media-manager";
+import {HttpMediaManager} from "../lib/http-media-manager";
 import HybridLoader from "../lib/hybrid-loader";
-import {LoaderEvents, Segment} from "../lib/loader-interface";
+import {Segment} from "../lib/loader-interface";
 import {anyFunction, anyOfClass, instance, mock, verify, when} from "ts-mockito";
 import * as assert from "assert";
 import {P2PMediaManager} from "../lib/p2p-media-manager";
@@ -28,7 +28,7 @@ describe("HybridLoader", () => {
         return httpDownloads.has(segment.id);
     });
     let httpSegmentLoadedListener: Function = () => {};
-    when(httpMediaManger.on(LoaderEvents.SegmentLoaded, anyFunction())).thenCall((event, listener) => {
+    when(httpMediaManger.on("segment-loaded", anyFunction())).thenCall((event, listener) => {
         httpSegmentLoadedListener = listener;
     });
 
@@ -77,14 +77,14 @@ describe("HybridLoader", () => {
             trackerAnnounce: [ "wss://tracker.btorrent.xyz/", "wss://tracker.openwebtorrent.com/" ]
         };
         const hybridLoader = new HybridLoader(settings);
-        verify(httpMediaManger.on(LoaderEvents.SegmentLoaded, anyFunction())).once();
+        verify(httpMediaManger.on("segment-loaded", anyFunction())).once();
 
         const segments: Segment[] = [
-            new Segment("uu1", "u1", 0),
-            new Segment("uu2", "u2", 1),
-            new Segment("uu3", "u3", 2),
-            new Segment("uu5", "u4", 3),
-            new Segment("uu4", "u5", 4)
+            new Segment("uu1", "u1", undefined, 0),
+            new Segment("uu2", "u2", undefined, 1),
+            new Segment("uu3", "u3", undefined, 2),
+            new Segment("uu5", "u4", undefined, 3),
+            new Segment("uu4", "u5", undefined, 4)
         ];
         const swarmId = "swarmId";
 
@@ -93,22 +93,22 @@ describe("HybridLoader", () => {
         assert.equal(httpDownloads.size, 1);
         let segment = httpDownloads.values().next().value;
         verify(httpMediaManger.download(segment)).once();
-        assert.deepEqual(segment, {id: segments[0].id, url: segments[0].url, priority: segments[0].priority, data: undefined, downloadSpeed: 0});
+        assert.deepEqual(segment, {id: segments[0].id, url: segments[0].url, range: undefined, priority: segments[0].priority, data: undefined, downloadSpeed: 0});
 
         // file loaded via http
         httpDownloads.clear();
-        httpSegmentLoadedListener(segments[0].id, segments[0].url, new ArrayBuffer(1));
+        httpSegmentLoadedListener(segments[0], new ArrayBuffer(1));
         assert.equal(httpDownloads.size, 1);
         segment = httpDownloads.values().next().value;
         verify(httpMediaManger.download(segment)).once();
-        assert.deepEqual(segment, {id: segments[1].id, url: segments[1].url, priority: segments[1].priority, data: undefined, downloadSpeed: 0});
+        assert.deepEqual(segment, {id: segments[1].id, url: segments[1].url, range: undefined, priority: segments[1].priority, data: undefined, downloadSpeed: 0});
 
         // load same files
         hybridLoader.load(segments, swarmId);
         assert.equal(httpDownloads.size, 1);
         segment = httpDownloads.values().next().value;
         verify(httpMediaManger.download(segment)).once();
-        assert.deepEqual(segment, {id: segments[1].id, url: segments[1].url, priority: segments[1].priority, data: undefined, downloadSpeed: 0});
+        assert.deepEqual(segment, {id: segments[1].id, url: segments[1].url, range: undefined, priority: segments[1].priority, data: undefined, downloadSpeed: 0});
     });
 
 });
