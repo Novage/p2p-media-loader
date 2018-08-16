1d0ec4ac151a29712250805fc45d5c6cd04a0f89
Stephan Hesse <tchakabam@gmail.com>
2018-08-16 17:50:56 +0200

    webpackfile: make paths/filenames nicer

--- p2p-media-loader-core/webpackfile.js
+++ p2p-media-loader-core/webpackfile.js
@@ -1,4 +1,8 @@
-function makeConfig({entry, mode}) {
+const path = require('path');
+
+const OUTPUT_PATH = 'build/webpack'
+
+function makeConfig({libName, entry, mode}) {
     return {
         mode,
         entry,
@@ -12,10 +16,21 @@ function makeConfig({entry, mode}) {
             // all files with a `.ts` extension will be handled by `ts-loader`
             { test: /\.ts?$/, loader: "ts-loader" },
           ]
-        }
+        },
+        output: {
+            filename: libName + '.umd.js',
+            chunkFilename: '[name].js',
+            path: path.resolve(__dirname, OUTPUT_PATH),
+            publicPath: '/' + OUTPUT_PATH + '/',
+            library: libName,
+            libraryTarget: 'umd',
+            libraryExport: 'default',
+            globalObject: 'this'
+        },
+        plugins: []
     }
 };
 
 module.exports = [
-    makeConfig({entry: './lib/index', mode: 'development'})
+    makeConfig({libName: 'P2pMediaLoaderCore', entry: './lib/index', mode: 'development'})
 ];
