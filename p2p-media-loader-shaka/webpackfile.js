ebc6ae244a472cad95483b28af3453e2b8aa2fea
Andriy Lysnevych <andriy.lysnevych@gmail.com>
2018-09-05 01:35:15 +0300

    Webpack build for shaka integration

---
+++ p2p-media-loader-shaka/webpackfile.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+const OUTPUT_PATH = 'build'
+
+function makeConfig({libName, entry, mode}) {
+    return {
+        mode,
+        entry,
+        resolve: {
+          // Add `.ts` as a resolvable extension.
+          extensions: [".ts", ".js"]
+        },
+        module: {
+          rules: [
+            // all files with a `.ts` extension will be handled by `ts-loader`
+            { test: /\.ts?$/, exclude: [/node_modules/], loader: "ts-loader" },
+          ]
+        },
+        output: {
+            filename: libName + '.js',
+            path: path.resolve(__dirname, OUTPUT_PATH)
+        },
+        externals: {
+            'p2p-media-loader-core': 'window.p2pml.core'
+        },
+        plugins: []
+    }
+};
+
+module.exports = [
+    makeConfig({entry: './lib/browser-init-webpack.js', mode: 'development', libName: 'p2p-media-loader-shaka', }),
+    makeConfig({entry: './lib/browser-init-webpack.js', mode: 'production', libName: 'p2p-media-loader-shaka.min'})
+];
