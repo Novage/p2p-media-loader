8c65c7b7157bfd1219908e69df50d3e668218651
Andriy Lysnevych <andriy.lysnevych@novage.com.ua>
2020-11-22 12:39:14 +0200

    Fix Webpack 5 build

--- p2p-media-loader-core/webpackfile.js
+++ p2p-media-loader-core/webpackfile.js
@@ -3,28 +3,44 @@ const webpack = require("webpack");
 
 const OUTPUT_PATH = "build";
 
-function makeConfig({libName, entry, mode}) {
+function makeConfig({ libName, entry, mode }) {
     return {
         mode,
         entry,
         resolve: {
-          // Add `.ts` as a resolvable extension.
-          extensions: [".ts", ".js"]
+            extensions: [".ts", ".js"],
         },
         module: {
-          rules: [
-            // all files with a `.ts` extension will be handled by `ts-loader`
-            { test: /\.ts?$/, exclude: [/node_modules/], loader: "ts-loader" },
-          ]
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/,
+                },
+            ],
         },
         output: {
             filename: libName + ".js",
-            path: path.resolve(__dirname, OUTPUT_PATH)
-        }
-    }
-};
+            path: path.resolve(__dirname, OUTPUT_PATH),
+        },
+        plugins: [
+            new webpack.ProvidePlugin({
+                Buffer: ["buffer", "Buffer"],
+                process: "process/browser",
+            }),
+        ],
+    };
+}
 
 module.exports = [
-    makeConfig({entry: "./lib/browser-init-webpack.ts", mode: "development", libName: "p2p-media-loader-core", }),
-    makeConfig({entry: "./lib/browser-init-webpack.ts", mode: "production", libName: "p2p-media-loader-core.min"})
+    makeConfig({
+        entry: "./lib/browser-init-webpack.ts",
+        mode: "development",
+        libName: "p2p-media-loader-core",
+    }),
+    makeConfig({
+        entry: "./lib/browser-init-webpack.ts",
+        mode: "production",
+        libName: "p2p-media-loader-core.min",
+    }),
 ];
