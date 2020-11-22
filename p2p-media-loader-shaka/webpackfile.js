a9fddc9d3bb25879e630a6f4f4ca23079ef66604
Andriy Lysnevych <andriy.lysnevych@novage.com.ua>
2020-11-22 20:21:19 +0200

    Make Webpack config more common

--- p2p-media-loader-shaka/webpackfile.js
+++ p2p-media-loader-shaka/webpackfile.js
@@ -3,33 +3,43 @@ const webpack = require("webpack");
 
 const OUTPUT_PATH = "build";
 
-function makeConfig({libName, entry, mode}) {
+function makeConfig({ libName, entry, mode }) {
     return {
         mode,
         entry,
         resolve: {
-          // Add `.ts` as a resolvable extension.
-          extensions: [".ts", ".js"]
+            extensions: [".ts", ".js"],
         },
         module: {
-          rules: [
-            // all files with a `.ts` extension will be handled by `ts-loader`
-            { test: /\.ts?$/, exclude: [/node_modules/], loader: "ts-loader" },
-          ]
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/,
+                },
+            ],
         },
         output: {
             filename: libName + ".js",
-            path: path.resolve(__dirname, OUTPUT_PATH)
+            path: path.resolve(__dirname, OUTPUT_PATH),
         },
         externals: {
             "p2p-media-loader-core": "window.p2pml.core",
-            "debug": "window.p2pml._shared.debug",
-            "events": "window.p2pml._shared.events",
-        }
-    }
-};
+            debug: "window.p2pml._shared.debug",
+            events: "window.p2pml._shared.events",
+        },
+    };
+}
 
 module.exports = [
-    makeConfig({entry: "./lib/browser-init.ts", mode: "development", libName: "p2p-media-loader-shaka", }),
-    makeConfig({entry: "./lib/browser-init.ts", mode: "production", libName: "p2p-media-loader-shaka.min"})
+    makeConfig({
+        entry: "./lib/browser-init.ts",
+        mode: "development",
+        libName: "p2p-media-loader-shaka",
+    }),
+    makeConfig({
+        entry: "./lib/browser-init.ts",
+        mode: "production",
+        libName: "p2p-media-loader-shaka.min",
+    }),
 ];
