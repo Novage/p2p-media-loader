e2578af876f74103906ca3ed59bafed06c6198cd
Andriy Lysnevych <andriy.lysnevych@novage.com.ua>
2023-05-05 13:20:37 +0300

    Fix eslint config and tsconfig

--- packages/p2p-media-loader-core/.eslintrc.cjs
+++ packages/p2p-media-loader-core/.eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ["../.eslintrc.common.cjs"],
+  extends: ["../../.eslintrc.common.cjs"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     tsconfigRootDir: __dirname,
