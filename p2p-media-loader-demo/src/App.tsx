ec832148e8e018eddba1e872560b73d5bb531822
Igor Zolotarenko <igor.zolotarenko@novage.com.ua>
2023-05-12 18:17:42 +0300

    Create dplayer hlsjs loader demo.

--- p2p-media-loader-demo/src/App.tsx
+++ p2p-media-loader-demo/src/App.tsx
@@ -1,25 +1,55 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Engine as HlsJSEngine } from "p2p-media-loader-hlsjs";
-import { Engine as ShakaEngine } from "p2p-media-loader-shaka";
+import DPlayer from "dplayer";
+
+const bigBunnyBuck = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8";
+const anotherVideo =
+  "https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8";
+const byteRangeVideo =
+  "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [displayVideo, setDisplayVideo] = useState<boolean>(true);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    const hls = HlsJSEngine.getHlsInstance();
+    if (!container || !hls) return;
+
+    const player = new DPlayer({
+      container: container,
+      video: {
+        url: bigBunnyBuck,
+        type: "customHls",
+        customType: {
+          customHls: (video: HTMLVideoElement) => {
+            hls.loadSource(video.src);
+            hls.attachMedia(video);
+          },
+        },
+      },
+    });
+  }, [containerRef, displayVideo]);
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div>
+    <div style={{ textAlign: "center" }}>
+      <div style={{ marginBottom: 20 }}>
+        <h1>This is HLS.JS Player Demo</h1>
         <button
-          onClick={() =>
-            setCount((count) => new HlsJSEngine().increment(count))
-          }
+          onClick={() => setDisplayVideo((prev) => !prev)}
+          style={{ fontSize: 20, padding: "10px 15px" }}
         >
-          count is {count}
+          {displayVideo ? "Hide" : "Show"}
         </button>
       </div>
-      <p>Click on the Vite and React logos to learn more</p>
-      <p>{ShakaEngine.getText()}</p>
-    </>
+
+      {displayVideo && (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <div ref={containerRef} style={{ width: 1000 }} />
+        </div>
+      )}
+    </div>
   );
 }
 
