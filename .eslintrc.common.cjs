745fceabf8159341048e698effcc3974364df575
Igor Zolotarenko <igor.zolotarenko@novage.com.ua>
2023-06-07 12:30:21 +0300

    Config eslint no-console rule.

--- .eslintrc.common.cjs
+++ .eslintrc.common.cjs
@@ -1,16 +1,17 @@
 module.exports = {
   root: true,
   env: {
-    es2021: true,
+    es2021: true
   },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended"
   ],
   plugins: ["@typescript-eslint", "prettier"],
   ignorePatterns: ["/.eslintrc.cjs", "/lib", "/dist", "/vite.config.ts"],
   rules: {
     "prettier/prettier": "error",
-  },
+    "no-console": "warn"
+  }
 };
