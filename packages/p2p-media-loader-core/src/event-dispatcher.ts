7408c873b7511b1b699c68a9d94bbbb5930bc71b
Igor Zolotarenko <zolotarenko.i.r@gmail.com>
2023-11-13 16:47:39 +0200

    Add event dispatcher class.

---
+++ packages/p2p-media-loader-core/src/event-dispatcher.ts
@@ -0,0 +1,30 @@
+export class EventDispatcher<
+  T extends { [key: string]: (...args: any) => any },
+  K extends keyof T = keyof T
+> {
+  private readonly listeners = new Map<keyof T, Set<T[K]>>();
+
+  subscribe(eventType: K, listener: T[K]) {
+    let eventListeners = this.listeners.get(eventType);
+    if (!eventListeners) {
+      eventListeners = new Set();
+      this.listeners.set(eventType, eventListeners);
+    }
+    eventListeners.add(listener);
+  }
+
+  unsubscribe(eventType: K, listener: T[K]) {
+    const eventListeners = this.listeners.get(eventType);
+    if (!eventListeners) return;
+    eventListeners.delete(listener);
+    if (!eventListeners.size) this.listeners.delete(eventType);
+  }
+
+  dispatch(eventType: K, ...args: Parameters<T[K]>) {
+    const eventListeners = this.listeners.get(eventType);
+    if (!eventListeners) return;
+    for (const listener of eventListeners) {
+      listener(args);
+    }
+  }
+}
