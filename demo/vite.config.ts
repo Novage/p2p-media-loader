e20a80640a0850bee5ad4028792221a051e9ff36
Andriy Lysnevych <andriy.lysnevych@novage.com.ua>
2024-01-10 17:53:40 +0200

    Open browser on demo start

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
+  server: { open: true, host: true },
   plugins: [nodePolyfills(), react()],
 });
