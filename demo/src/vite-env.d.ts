79ec170e0451ffc5f952241c8f7e46efa6c8d816
Dmytro Demchenko <91938357+DimaDemchenko@users.noreply.github.com>
2024-02-19 17:15:19 +0200

    peerId generation (#327)
    * peerId generation
    
    * improvements
    
    * vite-env
    
    * pnpm-lock
    
    * vite-env in demo
    
    * improvements

--- demo/src/vite-env.d.ts
+++ demo/src/vite-env.d.ts
@@ -1,1 +1,2 @@
 /// <reference types="vite/client" />
+declare const __VERSION__: string;
--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import corePackageJson from "../packages/p2p-media-loader-core/package.json";
 
 export default defineConfig({
+  define: {
+    __VERSION__: JSON.stringify(corePackageJson.version),
+  },
   server: { open: true, host: true },
   plugins: [nodePolyfills(), react()],
 });
