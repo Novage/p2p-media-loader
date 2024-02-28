ff4124ad09639669447aecbbe95b033c4abd2ba1
DimaDemchenko <demchenkodmytro73@gmail.com>
2024-02-28 17:00:47 +0200

    dependencies update

--- packages/p2p-media-loader-core/src/index.ts
+++ packages/p2p-media-loader-core/src/index.ts
@@ -4,3 +4,4 @@ export { Core } from "./core";
 export type * from "./types";
 export { CoreRequestError } from "./requests/engine-request";
 export type { EngineCallbacks } from "./requests/engine-request";
+export { debug } from "debug";
