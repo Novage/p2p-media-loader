1bfe68c5530dcf9f0d0320963da4719ca50681f6
DimaDemchenko <demchenkodmytro73@gmail.com>
2024-03-22 12:07:11 +0200

    updated build config

--- packages/p2p-media-loader-demo/vite.config.ts
+++ packages/p2p-media-loader-demo/vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from "vite";
+import { UserConfig, defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+const getESMConfig = ({ minify }: { minify: boolean }): UserConfig => {
+  return {
+    plugins: [react()],
+    build: {
+      emptyOutDir: false,
+      minify: minify ? "esbuild" : false,
+      sourcemap: true,
+      lib: {
+        name: "p2pml.demo",
+        fileName: (format) =>
+          `p2p-media-loader-demo.${format}${minify ? ".min" : ""}.js`,
+        formats: ["es"],
+        entry: "src/index.ts",
+      },
+      rollupOptions: {
+        external: ["react", "react-dom"],
+      },
+    },
+  };
+};
+
+export default defineConfig(({ mode }) => {
+  switch (mode) {
+    case "esm":
+      return getESMConfig({ minify: false });
+
+    case "esm-min":
+    default:
+      return getESMConfig({ minify: true });
+  }
 });
