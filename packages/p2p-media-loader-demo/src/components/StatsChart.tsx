4b0d93c233d23b03c3ccb4a29c11017891314164
DimaDemchenko <ipz215_ddo@student.ztu.edu.ua>
2024-03-31 15:45:32 +0300

    implemented chart with fake data

--- packages/p2p-media-loader-demo/src/components/StatsChart.tsx
+++ packages/p2p-media-loader-demo/src/components/StatsChart.tsx
@@ -5,23 +5,28 @@ import React, { useEffect, useRef, useState } from "react";
 import * as d3 from "d3";
 
 const generateInitialStackedData = () => {
+  const nowInSeconds = Math.floor(Date.now() / 1000);
   return Array.from({ length: 120 }, (_, i) => ({
-    date: new Date(Date.now() - (19 - i) * 1000 * 60 * 60 * 24), // 20 days of data
-    series1: Math.random() * 100,
-    series2: Math.random() * 100,
-    series3: Math.random() * -100,
+    date: nowInSeconds - (120 - i),
+    series1: 0,
+    series2: 0,
+    series3: 0,
   }));
 };
 
 const generateNewStackedDataItem = (data) => {
-  const lastDate = data[data.length - 1].date;
+  const lastSecond = data[data.length - 1].date;
   return {
-    date: new Date(lastDate.getTime() + 1000 * 60 * 60 * 24),
-    series1: Math.random() * 100,
-    series2: Math.random() * 100,
-    series3: Math.random() * 100,
+    date: lastSecond + 1,
+    series1: Math.random() * 10,
+    series2: Math.random() * 10,
+    series3: Math.random() * -10,
   };
 };
+
+const margin = { top: 20, right: 20, bottom: 30, left: 50 },
+  width = 710 - margin.left - margin.right,
+  height = 250 - margin.top - margin.bottom;
 
 export const MovingStackedAreaChart = () => {
   const [data, setData] = useState(generateInitialStackedData());
@@ -40,11 +45,6 @@ export const MovingStackedAreaChart = () => {
   useEffect(() => {
     if (!svgRef.current) return;
 
-    const margin = { top: 20, right: 20, bottom: 30, left: 50 },
-      width = 710 - margin.left - margin.right,
-      height = 250 - margin.top - margin.bottom;
-
-    // Set up the SVG container if it's not already there
     const svg = d3.select(svgRef.current);
     svg
       .selectAll("g")
@@ -55,16 +55,16 @@ export const MovingStackedAreaChart = () => {
 
     const content = svg.select("g");
 
-    const series = d3.stack().keys(["series1", "series2"])(data); // Only stack positive series
+    const series = d3.stack().keys(["series1", "series2"])(data);
 
     const maxStackedValue = d3.max(series, (serie) =>
       d3.max(serie, (d) => d[1]),
     );
-    const minSeries3Value = d3.min(data, (d) => d.series3); // Assuming series3 is always negative
+    const minSeries3Value = d3.min(data, (d) => d.series3);
+    const yTickValues = Math.round((maxStackedValue - minSeries3Value) / 5);
 
-    // Scales
     const xScale = d3
-      .scaleTime()
+      .scaleLinear()
       .domain(d3.extent(data, (d) => d.date))
       .range([0, width]);
     const yScale = d3
@@ -72,10 +72,8 @@ export const MovingStackedAreaChart = () => {
       .domain([minSeries3Value, maxStackedValue])
       .range([height, 0]);
 
-    // Colors
     const color = d3.scaleOrdinal(d3.schemeCategory10);
 
-    // Area generator
     const area = d3
       .area()
       .x((d) => xScale(d.data.date))
@@ -86,26 +84,24 @@ export const MovingStackedAreaChart = () => {
     const areaSeries3 = d3
       .area()
       .x((d) => xScale(d.date))
-      .y0(() => yScale(0)) // This now represents the starting point at the Y-axis line
-      .y1((d) => yScale(d.series3)) // Extend down to represent the negative value
-      .curve(d3.curveBasis); // Keeping the curve smooth
+      .y0(() => yScale(0))
+      .y1((d) => yScale(d.series3))
+      .curve(d3.curveBasis);
 
-    // Define the line generator
     const line = d3
       .line()
       .x((d) => xScale(d.data.date))
-      .y((d) => yScale(d[1])) // Use the y1 value for the line's y position
-      .curve(d3.curveBasis); // Ensure the line is smoothed
+      .y((d) => yScale(d[1]))
+      .curve(d3.curveBasis);
 
     const lineSeries3 = d3
       .line()
       .x((d) => xScale(d.date))
       .y((d) => yScale(d.series3))
-      .curve(d3.curveBasis); // Match the curve style of the area
+      .curve(d3.curveBasis);
 
-    // First, draw the axes (make sure this comes first)
-    content.selectAll(".axis").remove(); // Clear previous axes, if any
-    // X-axis
+    content.selectAll(".axis").remove();
+    // Axes
     content
       .append("g")
       .attr("class", "axis axis--x")
@@ -114,17 +110,37 @@ export const MovingStackedAreaChart = () => {
       .selectAll("line")
       .attr("stroke", "#ddd")
       .attr("stroke-dasharray", "2,2");
-    // Y-axis
+
+    console.log("yTickValues", yTickValues);
     content
       .append("g")
       .attr("class", "axis axis--y")
-      .call(d3.axisLeft(yScale).tickSize(-width))
+      .call(d3.axisLeft(yScale).tickSize(-width).ticks(yTickValues))
       .selectAll("line")
       .attr("stroke", "#ddd")
       .attr("stroke-dasharray", "2,2");
 
-    // Clear previous areas
+    // Data layers
     content.selectAll(".layer").remove();
+
+    content
+      .selectAll(".series3-area")
+      .data([data])
+      .join("path")
+      .attr("class", "series3-area")
+      .attr("d", areaSeries3)
+      .style("fill", "lightblue")
+      .style("opacity", 0.7);
+    content
+      .selectAll(".series3-line")
+      .data([data])
+      .join("path")
+      .attr("class", "series3-line")
+      .attr("d", lineSeries3)
+      .style("fill", "none")
+      .style("stroke", "blue")
+      .style("stroke-width", 2);
+
     content
       .selectAll(".layer")
       .data(series)
@@ -143,23 +159,10 @@ export const MovingStackedAreaChart = () => {
       .style("fill", "none")
       .style("stroke", (d, i) => color(i))
       .style("stroke-width", 1.5);
-    content
-      .selectAll(".series3-area")
-      .data([data]) // Ensure data is wrapped in an array for D3's area generator
-      .join("path")
-      .attr("class", "series3-area")
-      .attr("d", areaSeries3)
-      .style("fill", "lightblue") // Choose a distinctive fill color for series3
-      .style("opacity", 0.7);
-    content
-      .selectAll(".series3-line")
-      .data([data])
-      .join("path")
-      .attr("class", "series3-line")
-      .attr("d", lineSeries3)
-      .style("fill", "none")
-      .style("stroke", "blue") // Ensure it's visually distinct
-      .style("stroke-width", 2);
+
+    return () => {
+      svg.selectAll("*").remove();
+    };
   }, [data]);
 
   return <svg ref={svgRef} width={710} height={250}></svg>;
