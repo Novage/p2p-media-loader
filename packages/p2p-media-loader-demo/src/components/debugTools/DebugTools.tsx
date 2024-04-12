a8d0abd669e80eb6833bf2a36a94efd6b59a5b1d
DimaDemchenko <demchenkodmytro73@gmail.com>
2024-04-12 10:48:30 +0300

    vite bundler

--- packages/p2p-media-loader-demo/src/components/debugTools/DebugTools.tsx
+++ packages/p2p-media-loader-demo/src/components/debugTools/DebugTools.tsx
@@ -2,7 +2,7 @@ import { DebugSelector } from "./DebugSelector";
 
 export const DebugTools = () => {
   return (
-    <div className="container dev-container">
+    <div className="demo-container dev-container">
       <DebugSelector />
       <a href="modules-demo/index.html">ES modules demo</a>
     </div>
