8a7afe2b5b6f8f9df851b202e3877901d68f27fe
DimaDemchenko <demchenkodmytro73@gmail.com>
2024-05-02 15:59:00 +0300

    simplified vidstack player setup

--- packages/p2p-media-loader-demo/src/components/players/hlsjs/HlsjsVidstack.tsx
+++ packages/p2p-media-loader-demo/src/components/players/hlsjs/HlsjsVidstack.tsx
@@ -10,11 +10,11 @@ import {
   defaultLayoutIcons,
   DefaultVideoLayout,
 } from "@vidstack/react/player/layouts/default";
-
 import { PlayerProps } from "../../../types";
-import { HlsJsP2PEngine } from "p2p-media-loader-hlsjs";
+import { HlsJsP2PEngine, HlsWithP2PConfig } from "p2p-media-loader-hlsjs";
 import { configureHlsP2PEngineEvents } from "../utils";
-import { useCallback, useEffect, useRef } from "react";
+import { useCallback } from "react";
+import Hls from "hls.js";
 
 export const HlsjsVidstack = ({
   streamUrl,
@@ -24,56 +24,41 @@ export const HlsjsVidstack = ({
   onChunkDownloaded,
   onChunkUploaded,
 }: PlayerProps) => {
-  const engineRef = useRef<HlsJsP2PEngine | null>(null);
-
-  useEffect(() => {
-    engineRef.current = new HlsJsP2PEngine({
-      core: {
-        swarmId: "custom swarm ID for stream 2000341",
-        announceTrackers,
-      },
-    });
-
-    configureHlsP2PEngineEvents({
-      engine: engineRef.current,
-      onPeerConnect,
-      onPeerDisconnect,
-      onChunkDownloaded,
-      onChunkUploaded,
-    });
-
-    return () => {
-      if (engineRef.current) {
-        engineRef.current.destroy();
-        engineRef.current = null;
-      }
-    };
-  }, [
-    announceTrackers,
-    onChunkDownloaded,
-    onChunkUploaded,
-    onPeerConnect,
-    onPeerDisconnect,
-  ]);
-
   const onProviderChange = useCallback(
     (provider: MediaProviderAdapter | null) => {
-      if (isHLSProvider(provider) && engineRef.current) {
-        provider.library =
-          "https://cdnjs.cloudflare.com/ajax/libs/hls.js/1.5.7/hls.min.js";
+      if (isHLSProvider(provider)) {
+        const HlsWithP2P = HlsJsP2PEngine.injectMixin(Hls);
 
-        provider.config = {
-          ...engineRef.current.getHlsJsConfig(),
+        provider.library = HlsWithP2P;
+
+        const config: HlsWithP2PConfig<typeof HlsWithP2P> = {
+          p2p: {
+            core: {
+              swarmId: "custom swarm ID for stream 2000341",
+              announceTrackers,
+            },
+            onHlsJsCreated: (hls) => {
+              configureHlsP2PEngineEvents({
+                engine: hls.p2pEngine,
+                onPeerConnect,
+                onPeerDisconnect,
+                onChunkDownloaded,
+                onChunkUploaded,
+              });
+            },
+          },
         };
 
-        provider.onInstance((hls) => {
-          if (!engineRef.current) return;
-
-          engineRef.current.setHls(hls);
-        });
+        provider.config = config;
       }
     },
-    [],
+    [
+      announceTrackers,
+      onChunkDownloaded,
+      onChunkUploaded,
+      onPeerConnect,
+      onPeerDisconnect,
+    ],
   );
 
   return (
