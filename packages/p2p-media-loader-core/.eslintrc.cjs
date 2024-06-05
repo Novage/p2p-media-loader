ca107109295ff82b23e23ea9b779158fab7166d6
DimaDemchenko <demchenkodmytro73@gmail.com>
2024-06-05 10:52:29 +0300

    Add tests for core utils

--- packages/p2p-media-loader-core/.eslintrc.cjs
+++ packages/p2p-media-loader-core/.eslintrc.cjs
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   extends: ["../../.eslintrc.common.cjs"],
   parser: "@typescript-eslint/parser",
+  exclude: ["test/**/*"],
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: ["tsconfig.json", "tsconfig.node.json"],
---
+++ packages/p2p-media-loader-core/tests/utils.test.ts
@@ -0,0 +1,171 @@
+import { expect, test } from "vitest";
+import { overrideConfig } from "../src/utils/utils";
+import {
+  CommonCoreConfig,
+  CoreConfig,
+  DynamicCoreConfig,
+  StreamConfig,
+} from "../src/types";
+
+test("override configs", () => {
+  const coreConfig: CoreConfig = {
+    cachedSegmentExpiration: 1200,
+    cachedSegmentsCount: 0,
+    mainStream: {
+      simultaneousHttpDownloads: 3,
+    },
+  };
+
+  const override: CoreConfig = {
+    cachedSegmentExpiration: undefined,
+    secondaryStream: {
+      simultaneousHttpDownloads: 5,
+    },
+  };
+
+  const result: CoreConfig = {
+    cachedSegmentExpiration: undefined,
+    cachedSegmentsCount: 0,
+    mainStream: {
+      simultaneousHttpDownloads: 3,
+    },
+  };
+
+  expect(overrideConfig(coreConfig, override)).toEqual(result);
+});
+
+test("override common config", () => {
+  const commonConfig: CommonCoreConfig = {
+    cachedSegmentExpiration: 1200,
+    cachedSegmentsCount: 999,
+  };
+
+  const coreConfig: CoreConfig = {
+    cachedSegmentExpiration: undefined,
+    cachedSegmentsCount: 0,
+    simultaneousHttpDownloads: 3,
+    simultaneousP2PDownloads: 3,
+    highDemandTimeWindow: 15,
+    httpDownloadTimeWindow: 3000,
+    p2pDownloadTimeWindow: 6000,
+    webRtcMaxMessageSize: 64 * 1024 - 1,
+    p2pNotReceivingBytesTimeoutMs: 1000,
+    p2pInactiveLoaderDestroyTimeoutMs: 30 * 1000,
+    httpNotReceivingBytesTimeoutMs: 1000,
+    httpErrorRetries: 3,
+    p2pErrorRetries: 3,
+    trackerClientVersionPrefix: "PM1000",
+    announceTrackers: [
+      "wss://tracker.webtorrent.dev",
+      "wss://tracker.files.fm:7073/announce",
+      "wss://tracker.openwebtorrent.com",
+      // "wss://tracker.novage.com.ua",
+    ],
+    rtcConfig: {
+      iceServers: [
+        { urls: "stun:stun.l.google.com:19302" },
+        { urls: "stun:global.stun.twilio.com:3478" },
+      ],
+    },
+    validateP2PSegment: undefined,
+    httpRequestSetup: undefined,
+    swarmId: undefined,
+  };
+
+  const result: CommonCoreConfig = {
+    cachedSegmentExpiration: undefined,
+    cachedSegmentsCount: 0,
+  };
+
+  expect(overrideConfig(commonConfig, coreConfig)).toEqual(result);
+});
+
+test("override defined stream config", () => {
+  const mainStreamConfig: StreamConfig = {
+    simultaneousHttpDownloads: 3,
+    simultaneousP2PDownloads: 3,
+    highDemandTimeWindow: 15,
+    httpDownloadTimeWindow: 3000,
+    p2pDownloadTimeWindow: 6000,
+    webRtcMaxMessageSize: 64 * 1024 - 1,
+    p2pNotReceivingBytesTimeoutMs: 1000,
+    p2pInactiveLoaderDestroyTimeoutMs: 30 * 1000,
+    httpNotReceivingBytesTimeoutMs: 1000,
+    httpErrorRetries: 3,
+    p2pErrorRetries: 3,
+    trackerClientVersionPrefix: "PM1000",
+    announceTrackers: [
+      "wss://tracker.webtorrent.dev",
+      "wss://tracker.files.fm:7073/announce",
+      "wss://tracker.openwebtorrent.com",
+      // "wss://tracker.novage.com.ua",
+    ],
+    rtcConfig: {
+      iceServers: [
+        { urls: "stun:stun.l.google.com:19302" },
+        { urls: "stun:global.stun.twilio.com:3478" },
+      ],
+    },
+    validateP2PSegment: async () => true,
+    httpRequestSetup: undefined,
+    swarmId: undefined,
+  };
+
+  const dynamicCoreConfig: DynamicCoreConfig = {
+    cachedSegmentExpiration: 1200,
+    highDemandTimeWindow: 45,
+    httpDownloadTimeWindow: 5000,
+    p2pDownloadTimeWindow: 10000,
+    p2pNotReceivingBytesTimeoutMs: 2000,
+    p2pInactiveLoaderDestroyTimeoutMs: 15 * 1000,
+    httpNotReceivingBytesTimeoutMs: 1500,
+    httpErrorRetries: 2,
+    p2pErrorRetries: 4,
+    mainStream: {
+      simultaneousHttpDownloads: 2,
+      simultaneousP2PDownloads: 20,
+      validateP2PSegment: undefined,
+    },
+    secondaryStream: {
+      simultaneousHttpDownloads: 1,
+      simultaneousP2PDownloads: 2,
+    },
+  };
+
+  const result: StreamConfig = {
+    simultaneousHttpDownloads: 2,
+    simultaneousP2PDownloads: 20,
+    highDemandTimeWindow: 45,
+    httpDownloadTimeWindow: 5000,
+    p2pDownloadTimeWindow: 10000,
+    webRtcMaxMessageSize: 64 * 1024 - 1,
+    p2pNotReceivingBytesTimeoutMs: 2000,
+    p2pInactiveLoaderDestroyTimeoutMs: 15 * 1000,
+    httpNotReceivingBytesTimeoutMs: 1500,
+    httpErrorRetries: 2,
+    p2pErrorRetries: 4,
+    trackerClientVersionPrefix: "PM1000",
+    announceTrackers: [
+      "wss://tracker.webtorrent.dev",
+      "wss://tracker.files.fm:7073/announce",
+      "wss://tracker.openwebtorrent.com",
+      // "wss://tracker.novage.com.ua",
+    ],
+    rtcConfig: {
+      iceServers: [
+        { urls: "stun:stun.l.google.com:19302" },
+        { urls: "stun:global.stun.twilio.com:3478" },
+      ],
+    },
+    validateP2PSegment: undefined,
+    httpRequestSetup: undefined,
+    swarmId: undefined,
+  };
+
+  function overrideConfigs(config: StreamConfig, override: DynamicCoreConfig) {
+    overrideConfig(config, override);
+    return overrideConfig(config, override.mainStream!);
+  }
+
+  expect(overrideConfigs(mainStreamConfig, dynamicCoreConfig)).toEqual(result);
+});
