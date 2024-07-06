6ab020db9efbc3ff10ba38944366dfb3759ad687
Dmytro Demchenko <91938357+DimaDemchenko@users.noreply.github.com>
2024-07-06 11:27:49 +0300

    Refactor useScripts.ts (#390)

--- packages/p2p-media-loader-demo/src/hooks/useScripts.ts
+++ packages/p2p-media-loader-demo/src/hooks/useScripts.ts
@@ -1,37 +1,57 @@
 import { useEffect } from "react";
 
-const loadScript = (url: string): Promise<HTMLScriptElement> => {
+const generateUniqueId = () => {
+  return `script-${Math.random().toString(36).substring(2, 11)}`;
+};
+
+const loadScript = (script: { url: string; id: string }) => {
+  const { url, id } = script;
+
   return new Promise((resolve, reject) => {
-    const script = document.createElement("script");
-    script.src = url;
-    script.async = true;
-    script.type = "text/javascript";
-    script.onload = () => resolve(script);
-    script.onerror = (error) => reject(error);
-    document.body.appendChild(script);
+    const scriptElement = document.createElement("script");
+
+    scriptElement.src = url;
+    scriptElement.async = true;
+    scriptElement.type = "text/javascript";
+    scriptElement.setAttribute("data-id", id);
+    scriptElement.onload = () => resolve(scriptElement);
+    scriptElement.onerror = (error) => reject(error);
+
+    document.body.appendChild(scriptElement);
   });
 };
+
 export const useScripts = (scripts: string[]) => {
   useEffect(() => {
     let isCleanup = false;
 
-    const loadAllScripts = async () => {
-      for (const script of scripts) {
-        await loadScript(script);
-        if (isCleanup) break;
-      }
-    };
+    const scriptsWithIds = scripts.map((script) => ({
+      url: script,
+      id: generateUniqueId(),
+    }));
 
-    void loadAllScripts();
-
-    return () => {
+    const cleanUp = () => {
       isCleanup = true;
-      scripts.forEach((url) => {
-        const scriptElement = document.querySelector(`script[src="${url}"]`);
+      scriptsWithIds.forEach(({ id }) => {
+        const scriptElement = document.querySelector(`script[data-id="${id}"]`);
         if (scriptElement) {
           document.body.removeChild(scriptElement);
         }
       });
     };
+
+    const loadAllScripts = async () => {
+      for (const script of scriptsWithIds) {
+        await loadScript(script);
+        if (isCleanup) {
+          cleanUp();
+          break;
+        }
+      }
+    };
+
+    void loadAllScripts();
+
+    return () => cleanUp();
   }, [scripts]);
 };
