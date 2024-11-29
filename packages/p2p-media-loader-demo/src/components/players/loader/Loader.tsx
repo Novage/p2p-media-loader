17471aef89d44d18868a20d03dad7057a0084db1
DimaDemchenko <demchenkodmytro73@gmail.com>
2024-11-29 11:05:27 +0200

    fix: shaka player components

---
+++ packages/p2p-media-loader-demo/src/components/players/loader/Loader.tsx
@@ -0,0 +1,10 @@
+// Loader.tsx
+import "./loader.css";
+
+export const Loader = () => {
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+    </div>
+  );
+};
